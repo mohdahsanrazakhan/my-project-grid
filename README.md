3cc4d449333ce2bd4d66f390539fd3053f4496a0
Mark <datawithmark@gmail.com>
2024-02-05 16:01:10 +0530

    Update favicon and README

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# myProjectGrid
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is my project grid website there I post some of my pojects which I made till now.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Visit: [myProjectGrid](https://myprojectgrid.netlify.app)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="bg-gray-100 dark:bg-slate-700 flex items-center transition-all duration-500 ease-in-out">
       <div className='flex justify-center items-center mr-auto mx-3 gap-2'>
         <IoGrid size={24} color='skyblue' />
-        <h1 className="text-black dark:text-white text-xl flex justify-center items-center m-auto text-center font-medium transition-all duration-500 ease-in-out">Project Collection</h1>
+        <h1 className="text-black dark:text-white text-xl flex justify-center items-center m-auto text-center font-medium transition-all duration-500 ease-in-out">Project Grid</h1>
       </div>
       <div className='p-2 m-3 flex justify-center items-center border-2 rounded-md dark:border-slate-500'>
         <Switcher />
